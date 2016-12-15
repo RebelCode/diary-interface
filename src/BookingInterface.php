4d28520159cf7b8d9367a9d35908765c0e882924
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-15 18:52:00 +0100

    Added `BookingInterface`

---
+++ src/BookingInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RebelCode\Diary;
+
+/**
+ * Describes any object that can represent a booked period of time.
+ *
+ * @since [*next-version*]
+ */
+interface BookingInterface extends ModelInterface, PeriodInterface
+{
+}
