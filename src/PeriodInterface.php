b038069a3f0cbb359f3da65197afbba3d1970ae9
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-15 16:59:39 +0100

    Added `PeriodInterface`

---
+++ src/PeriodInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RebelCode\Diary;
+
+/**
+ * Describes any object that represents a particular period of time.
+ *
+ * @since [*next-version*]
+ */
+interface PeriodInterface
+{
+    /**
+     * Gets the datetime for the start of the period.
+     *
+     * @since [*next-version*]
+     *
+     * @return \DateTimeInterface
+     */
+    public function getStart();
+
+    /**
+     * Gets the datetime for the end of the period.
+     *
+     * @since [*next-version*]
+     *
+     * @return \DateTimeInterface
+     */
+    public function getEnd();
+}
