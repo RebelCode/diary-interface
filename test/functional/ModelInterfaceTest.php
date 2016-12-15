4d8b68d508b9372d33c680b7f195b576ff3824a8
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-15 18:26:54 +0100

    Added test for `ModelInterface`

---
+++ test/functional/ModelInterfaceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace RebelCode\Diary\Test;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \RebelCode\Diary\ModelInterface}.
+ *
+ * @since [*next-version*]
+ */
+class ModelInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\RebelCode\\Diary\\ModelInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \RebelCode\Diary\ModelInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getId()
+            ->getResourceModel()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+}
