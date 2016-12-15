5836cbbe74118605070714e06779d533aa9b9e2d
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-15 18:52:13 +0100

    Added test for `BookingInterface`

---
+++ test/functional/BookingInterfaceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RebelCode\Diary\Test;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \RebelCode\Diary\BookingInterface}.
+ *
+ * @since [*next-version*]
+ */
+class BookingInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\RebelCode\\Diary\\BookingInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \RebelCode\Diary\BookingInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getId()
+            ->getStart()
+            ->getEnd()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+        $this->assertInstanceOf('\\RebelCode\\Diary\\PeriodInterface', $subject);
+        $this->assertInstanceOf('\\RebelCode\\Diary\\ModelInterface', $subject);
+    }
+}
