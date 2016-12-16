ff7fddab5bd51441c9e8de25df4d91dd219283b4
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-16 13:37:02 +0100

    Added test for `StorageAdapterInterface`

---
+++ test/functional/Storage/StorageAdapterInterfaceTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace RebelCode\Diary\Test\Storage;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \RebelCode\Diary\Storage\StorageAdapterInterface}.
+ *
+ * @since [*next-version*]
+ */
+class StorageAdapterInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\RebelCode\\Diary\\Storage\\StorageAdapterInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \RebelCode\Diary\Storage\StorageAdapterInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getRecord()
+            ->getRecords()
+            ->hasRecord()
+            ->hasRecords()
+            ->deleteRecord()
+            ->deleteRecords()
+            ->insertRecord()
+            ->insertRecords()
+            ->query()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @covers \RebelCode\Diary\Storage\StorageAdapterInterface
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+}
