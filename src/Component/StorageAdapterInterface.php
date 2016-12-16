bd0855ba6e5e9ee838de005558f430239352a19d
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-16 14:52:12 +0100

    Fixed short parameter names

--- src/Component/StorageAdapterInterface.php
+++ src/Component/StorageAdapterInterface.php
@@ -12,58 +12,58 @@ interface StorageAdapterInterface
     /**
      * Gets the record with a given ID.
      *
-     * @param int|string $id The unique ID of the record.
+     * @param int|string $recordId The unique ID of the record.
      *
      * @return mixed The fetched record.
      */
-    public function getRecord($id);
+    public function getRecord($recordId);
 
     /**
      * Gets the records with a given IDs.
      *
-     * @param array $ids An array The unique IDs of the records to fetch.
+     * @param array $recordIds An array The unique IDs of the records to fetch.
      *
      * @return array An associative array with the IDs as keys and the fetched records as values.
      *               Records that could not be fetched will have their entry's value set to null.
      */
-    public function getRecords($ids);
+    public function getRecords($recordIds);
 
     /**
      * Checks if a record with the given ID exists.
      *
-     * @param int|string $id The unique ID of the record.
+     * @param int|string $recordId The unique ID of the record.
      *
      * @return bool True if a record with the given ID exists, false if not.
      */
-    public function hasRecord($id);
+    public function hasRecord($recordId);
 
     /**
      * Checks if records with the given IDs exist.
      *
-     * @param array $ids An array The unique IDs of the records.
+     * @param array $recordIds An array The unique IDs of the records.
      *
      * @return array An associative array of ID and boolean pairs.
      *               Each boolean value will be true if the record with the corresponding ID exists and false if not.
      */
-    public function hasRecords(array $ids);
+    public function hasRecords(array $recordIds);
 
     /**
      * Deletes a record with the given ID.
      *
-     * @param int|string $id The unique ID of the record.
+     * @param int|string $recordId The unique ID of the record.
      *
      * @return bool True if a record with the given ID exists and was deleted, false otherwise.
      */
-    public function deleteRecord($id);
+    public function deleteRecord($recordId);
 
     /**
      * Deletes the records with the given IDs.
      *
-     * @param array $ids The unique ID of the record.
+     * @param array $recordIds The unique ID of the record.
      *
      * @return bool True if a record with the given ID exists and was deleted, false otherwise.
      */
-    public function deleteRecords($ids);
+    public function deleteRecords($recordIds);
 
     /**
      * Inserts a record into storage.
