ec88bbbed2de1625bc3ecc210db50169cbcaa36a
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-19 15:15:46 +0100

    Added test for `DateTimeInterface`

---
+++ test/functional/DateTime/DateTimeInterfaceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace RebelCode\Diary\Test\DateTime;
+
+use \Xpmock\TestCase;
+
+/**
+ * Tests {@see \RebelCode\Diary\DateTime\DateTimeInterface}.
+ *
+ * @since [*next-version*]
+ */
+class DateTimeInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\RebelCode\\Diary\\DateTime\\DateTimeInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \RebelCode\Diary\DateTime\DateTimeInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getTimestamp()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @covers \RebelCode\Diary\DateTime\DateTimeInterface
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+}
