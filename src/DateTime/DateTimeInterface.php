b45110bc42352ba70efa222ba808eac78b8788e9
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-19 15:13:53 +0100

    Added `DateTimeInterface`

---
+++ src/DateTime/DateTimeInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RebelCode\Diary\DateTime;
+
+/**
+ * Any object that can represent dates and times.
+ *
+ * @since [*next-version*]
+ */
+interface DateTimeInterface
+{
+    /**
+     * Gets the Unix timestamp.
+     *
+     * @since [*next-version*]
+     *
+     * @return int The Unix timestamp representing the date.
+     */
+    public function getTimestamp();
+}
