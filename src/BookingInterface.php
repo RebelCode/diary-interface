f3805a24e21a208907a5da320a417fcdb2248b34
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-22 14:42:44 +0100

    Removed `BookingInterface::getMeta()`

--- src/BookingInterface.php
+++ src/BookingInterface.php
@@ -17,16 +17,4 @@ interface BookingInterface extends PeriodInterface
      * @return int The integer ID.
      */
     public function getId();
-
-    /**
-     * Gets the meta data for this booking.
-     *
-     * @since [*next-version*]
-     *
-     * @param string $key [optional] The key of the meta data entry to return. Default: null
-     *
-     * @return array If $key is null, an associative array of meta data keys and values.
-     *               If $key is a string, the meta value for that key or null if the key does not exist.
-     */
-    public function getMeta($key = null);
 }
