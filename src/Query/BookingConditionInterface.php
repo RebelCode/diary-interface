1969c880b93d8e81a0c9c53eb3daf01080f65ea5
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-28 12:07:50 +0100

    Added `BookingConditionInterface`

---
+++ src/Query/BookingConditionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RebelCode\Diary\Query;
+
+use Dhii\Espresso\BooleanExpressionInterface;
+
+/**
+ * An object that represents a booking query condition.
+ *
+ * @since [*next-version*]
+ */
+interface BookingConditionInterface extends BooleanExpressionInterface
+{
+}
