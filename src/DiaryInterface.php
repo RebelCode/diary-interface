5889fb9b529b85fe657f9d54918342c678e914f8
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-28 12:15:14 +0100

    Fixed uses in `DiaryInterface`

--- src/DiaryInterface.php
+++ src/DiaryInterface.php
@@ -2,7 +2,7 @@
 
 namespace RebelCode\Diary;
 
-use \RebelCode\Diary\Query\BookingQueryInterface;
+use RebelCode\Diary\Query\BookingQueryInterface;
 
 /**
  * Represents the hub of the Diary library.
