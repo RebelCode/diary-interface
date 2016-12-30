a179d97fbe51c83937cd29b0953588b81e6f0e5a
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-30 13:06:10 +0100

    Added `BookingPropertyTermInterface`

---
+++ src/Query/Condition/BookingPropertyTermInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RebelCode\Diary\Query\Condition;
+
+/**
+ * An entity property evaluable term  that is specific to booking entities.
+ *
+ * @since [*next-version*]
+ */
+interface BookingPropertyTermInterface extends EntityPropertyTermInterface
+{
+}
