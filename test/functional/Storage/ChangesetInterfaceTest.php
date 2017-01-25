05db02964267d87fdf996c492f32a56c48f43f09
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-25 17:16:18 +0100

    Added `ChangesetInterface`

---
+++ test/functional/Storage/ChangesetInterfaceTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace RebelCode\Diary\FuncTest\Storage;
+
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see RebelCode\Diary\Storage\ChangesetInterface}.
+ *
+ * @since [*next-version*]
+ */
+class ChangesetInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\\Diary\\Storage\\ChangesetInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return RebelCode\Diary\Storage\ChangesetInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getChanges()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+}
