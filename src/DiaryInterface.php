9cb5e4d7d1b736ad72cc98231ffb3ee55d48c240
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-27 16:21:11 +0100

    `DiaryInterface#get()` returns false on failure
    Changed from `null` for consistency with the other methods

--- src/DiaryInterface.php
+++ src/DiaryInterface.php
@@ -19,7 +19,7 @@ interface DiaryInterface
      *
      * @param QueryInterface $query The query that defines the criteria.
      *
-     * @return BookingInterface[]|null An array of booking instances or null on failure.
+     * @return BookingInterface[]|bool An array of booking instances or false on failure.
      */
     public function get(QueryInterface $query);
 
